b9f09453d1915fd76c515a0d9aac739eb6566655
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2026-03-06 19:24:46 +0200

    #454 - updated tests and setup lint & prettier

--- tests/jest-transform.js
+++ tests/jest-transform.js
@@ -9,7 +9,7 @@ module.exports = {
       /import\.meta\.url/g,
       `require('url').pathToFileURL(__filename).href`
     );
-    
+
     // Use ts-jest for actual transformation
     const tsJest = require('ts-jest');
     return tsJest.default.process(transformed, sourcePath, {
@@ -21,4 +21,3 @@ module.exports = {
     });
   },
 };
-
