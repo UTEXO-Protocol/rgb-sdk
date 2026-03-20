fbee8852df76f6070add4213dbb4bc4d3e60131c
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2026-03-20 19:28:57 +0200

    clean up logs

--- src/binding/NodeRgbLibBinding.ts
+++ src/binding/NodeRgbLibBinding.ts
@@ -182,9 +182,6 @@ export class NodeRgbLibBinding implements IRgbLibBinding {
         DEFAULT_INDEXER_URLS[this.network] || DEFAULT_INDEXER_URLS.signet;
     }
 
-    // console.log('dataDir', this.dataDir);
-    console.log("params", params);
-
     if (!fs.existsSync(this.dataDir)) {
       fs.mkdirSync(this.dataDir, { recursive: true });
     }
